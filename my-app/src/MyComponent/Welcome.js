1658f6ce76d32fe28b1922920d6d31a969c80033
susmita1994 <80892723+susmita1994@users.noreply.github.com>
2021-05-20 17:03:17 +0530

    button click event in class component
    button click event in class component

--- my-app/src/MyComponent/Welcome.js
+++ my-app/src/MyComponent/Welcome.js
@@ -1,8 +1,16 @@
 import React,{Component} from 'react';
 
 class Welcome extends Component{
+
+     DoThisFromClass() {
+        alert('Button clicked from class')
+    }
+
     render(){
-        return <h1>I am from {this.props.name}</h1>
+        return  <div>
+            <button onClick={this.DoThisFromClass}>Click Me From Class </button>
+            <h1>I am from {this.props.name}</h1>
+        </div> 
     }
 }
 
