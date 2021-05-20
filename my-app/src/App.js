56f3f8c9597f44164e32187c984459bdf485a42e
susmita1994 <80892723+susmita1994@users.noreply.github.com>
2021-05-20 18:13:58 +0530

    Arrow function
    Arrow function

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import Hello from './MyComponent/Hello';
 import Welcome from './MyComponent/Welcome';
+import Arrow from './MyComponent/Arrow';
 
 function App() {
   return (
     <div className="App">
-      <Hello name="susmita"></Hello>
-      <Welcome name="Class 1"></Welcome>
+      {/* <Hello name="susmita"></Hello>
+      <Welcome name="Class 1"></Welcome> */}
+      <Arrow></Arrow>
     
     </div>
   );
---
+++ my-app/src/MyComponent/Arrow.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+
+// function Arrow(){
+
+//     return(
+//         <button className="btn btn-danger">My Button</button>
+//     );
+// }
+
+const Arrow=()=>{
+    return(
+        <button className="btn btn-danger">My Button</button>
+    );
+}
+
+
+
+export default Arrow;
