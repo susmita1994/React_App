3b3fcac4816be1f4692a1d16620fbcb3da328aa1
susmita1994 <80892723+susmita1994@users.noreply.github.com>
2021-05-27 18:16:17 +0530

    validation signup form
    validation signup form

--- my-app/src/MyComp/Signup.js
+++ my-app/src/MyComp/Signup.js
@@ -15,9 +15,39 @@ class Signup extends Component {
         var inputName=event.target.name;
         var inputValue=event.target.value;
         this.setState({[inputName]:inputValue});
+
+        if (inputName==="fname") {
+
+           var namePattern=/^([a-zA-Z]){2,30}$/;
+            if(!namePattern.test(inputValue)){
+                this.setState({fname:"First Name is not valid"});
+            }
+        }
+        
+        if (inputName==="lname") {
+            var namePattern=/^([a-zA-Z]){2,30}$/;
+            if(!namePattern.test(inputValue)){
+                this.setState({lname:"Last Name is not valid"});
+            }
+        }
+
+        if (inputName==="email") {
+            var emailPattern=/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+            if(!emailPattern.test(inputValue)){
+                this.setState({email:"Email is not valid"});
+            }
+
+        }
+
+        if(inputName==="mobile"){
+            if(!Number(inputValue)){
+                this.setState({mobile:"Mobile No is not valid"});
+            }
+        }
+        
     }
 
-    render() {
+    render(){
         return (
             <div>
                 <p>First Name:{this.state.fname}</p>
@@ -26,13 +56,9 @@ class Signup extends Component {
                 <p>Mobile: {this.state.mobile}</p>
 
                 <form >
-                    <label> First Name : </label>
                     <input name="fname" onChange={this.onChangeHandler}  type="text" placeholder="First Name"></input> <br></br><br></br>
-                    <label>Last Name : </label>
                     <input name="lname" onChange={this.onChangeHandler} type="text" placeholder="Last Name"></input> <br></br> <br></br>
-                    <label> Email : </label>
                     <input name="email" onChange={this.onChangeHandler} type="text" placeholder="Email"></input> <br></br> <br></br>
-                    <label> Mobile : </label>
                     <input name="mobile" onChange={this.onChangeHandler} type="text" placeholder="Mobile"></input> <br></br> <br></br>
                     <input name="submit" type="submit" className="btn btn-primary" value="Signup"></input>
                 </form>
