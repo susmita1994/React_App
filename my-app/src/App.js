257b236fd535d704316aa8409c7c85048f6a4caf
susmita1994 <80892723+susmita1994@users.noreply.github.com>
2021-05-31 11:55:24 +0530

    use css module
    use css module

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import Style from'./Mycss.module.css';
 
 function App() {
   return (
     <div className="App">
 
-        <h1 className="TextStyle">Susmita Dhua</h1>
+        <h1 className={Style.TextStyle}>Susmita Dhua</h1>
     </div>
   );
 }
